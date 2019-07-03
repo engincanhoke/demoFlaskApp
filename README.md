c60d81ab5c059e806ba149851073e0ae7def2419
bcfmmert <mert@bestcloudfor.me>
2019-07-03 18:58:08 +0300

    chart && helm command added

---
+++ README.md
@@ -0,0 +1,4 @@
+ 
+# Command
+
+    $ helm install flask --name flaskdemo --set image.repository=flaskdemo --set image.tag=latest --set service.name=flaskdemo 
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ az login --service-principal -u $SERVICE_PRINCIPAL_ID -p $SERVICE_PRINCIPAL_SECR
 echo "Getting credentials for cluster..."
 az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP_NAME
 
-kubectl get nodes
+helm install flask --name flaskdemo --set image.repository=mertongngl/flaskdemo --set image.tag=latest --set service.name=flaskdemo --namespace travistest
 
 echo "Helm List --------------------"
 helm list
