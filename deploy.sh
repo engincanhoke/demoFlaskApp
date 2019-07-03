2c51eff53d9c17cc8edaab069231066852c9b4f5
bcfmmert <mert@bestcloudfor.me>
2019-07-03 16:41:22 +0300

    deploy.sh created && travis.yml edited

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+IFS=$'\n\t'
+
+az login --service-principal -u $SERVICE_PRINCIPAL_ID -p $SERVICE_PRINCIPAL_SECRET --tenant $TENANT 1> /dev/null
+
+echo "Getting credentials for cluster..."
+az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP_NAME
+
+kubectl get nodes
+
+echo "Helm List --------------------"
+helm list
